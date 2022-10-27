ad76f0ca8955c9400efc64f57fe19c04f2c8bd47
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-10-27 17:04:39 -0400

    putting the static file public in the app,the listen method, and the default 404 if none of the route exist

--- app.mjs
+++ app.mjs
@@ -4,3 +4,18 @@ const nasdaq = new Nasdaq();
 let jsonFile = await nasdaq.getNasdaq();
 const app = express();
 const port = 3000;
+//static public 
+app.use(express.static('public'));
+
+
+
+
+//use incase there is nothing found
+app.use((req, res)=>{
+  res.status(404).send("NOT_FOUND")
+});
+
+//port to listen to, and a prompt for which port is listening to
+app.listen(port, ()=>{
+  console.log(`Web server listening to: ${port}`);
+})
