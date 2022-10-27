e4a8d1457b3d060c61beb14aea92f47a1ee765bc
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-10-27 14:32:04 -0400

    made the basic i/o functions to read a file

---
+++ fileio/fileio.mjs
@@ -0,0 +1,34 @@
+import fs from 'node:fs/promises';
+import * as checkFile from './checkFile.mjs';
+
+async function readFile(path) {
+  if(await checkFile.checkAccess(path)) {
+      if (await checkFile.checkIfFile(path)) {
+          try {
+              let data = fs.readFile(path, "utf-8");
+              return data;
+          }catch(err) {
+              console.error(err);
+          }
+          
+      }
+      throw new Error("Something went wrong when trying to read the file");
+  }
+}
+
+/**
+ * function that parse the JSON into object
+ * @param {string} path 
+ * @returns object
+ */
+async function readJSON(path) {
+  try {
+      let data = await readFile(path);
+      return JSON.parse(data);
+  } catch (error) {
+      console.error(error);
+      return "";
+  }
+}
+
+export {readFile, readJSON}
