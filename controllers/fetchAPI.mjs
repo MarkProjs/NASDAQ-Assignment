d0f388e96022375345ecefc7fb1d6b838ede4c0d
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-10-27 15:15:45 -0400

    made the the fetch API controller and deleted the keys.mjs because it is not suppose to be in the gitlab

--- controllers/fetchAPI.mjs
+++ controllers/fetchAPI.mjs
@@ -1,5 +1,13 @@
 import key from './keys.mjs';
 
 export async function fetchAPI(symbol) {
-  
+  let url = `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${key.key}`;
+  let response = await fetch(url);
+  let content;
+  if (response.ok) {
+    content = await response.json();
+  }else {
+    throw new Error ("Status code: " + response.status);
+  }
+ return content;
 }
--- controllers/keys.mjs
+++
@@ -1,1 +0,0 @@
-export const key = "cddci5aad3iag7bhkblgcddci5aad3iag7bhkbm0"
