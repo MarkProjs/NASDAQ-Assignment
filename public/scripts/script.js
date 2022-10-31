78cde3f5c29aa03ce0f9db0dd19029665f06ed8d
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-10-31 14:25:45 -0400

    changing the spacing becuase of the eslint issue

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+'use strict'
 document.addEventListener("DOMContentLoaded", setup)
 
 //submit button
@@ -7,26 +9,24 @@ let button = document.querySelector("#submit");
  * function to call if the DOM is loaded
  */
 async function setup(){
-    await getData().catch(e => {
-        console.error(e.message)
-    });
+  await getData().catch(e => {
+    console.error(e.message)
+  });
 }
 
 /**
  * function that fetches the whole json and pass it to the populateDatalist function
  */
 async function getData(){
-    let url = 'http://localhost:3000/api'
-    let response = await fetch(url);
-    let content;
-    if(response.ok) {
-        content = await response.json();
-    }
-    else {
-        throw new Error("Status code: " + response.status);
-    }
-
-    populateDatalist(content);
+  let url = 'http://localhost:3000/api'
+  let response = await fetch(url);
+  let content;
+  if(response.ok) {
+    content = await response.json();
+  }else {
+    throw new Error("Status code: " + response.status);
+  }
+  populateDatalist(content);
 }
 
 /**
@@ -34,23 +34,23 @@ async function getData(){
  * @param {object} json 
  */
 function populateDatalist(json) {
-    let datalist = document.querySelector('#symbols');
-    for (let element of json){
-        let newData = document.createElement('option');
-        newData.value = element.symbol;
-        datalist.appendChild(newData);
-    }
+  let datalist = document.querySelector('#symbols');
+  for (let element of json){
+    let newData = document.createElement('option');
+    newData.value = element.symbol;
+    datalist.appendChild(newData);
+  }
 }
 
 //Event listner for the submit button
 button.addEventListener("click", () => {
-    let nasdaq = document.querySelector("#symbol-choice").value;
-    fetchStock(nasdaq)
+  let nasdaq = document.querySelector("#symbol-choice").value;
+  fetchStock(nasdaq)
     .catch((err) => {
-        let currentPrice = document.querySelector('#current_price');
-        currentPrice.textContent = "ERROR: can't have empty symbol"
-        console.error(err.message)
-      });
+      let currentPrice = document.querySelector('#current_price');
+      currentPrice.textContent = "ERROR: can't have empty symbol"
+      console.error(err.message)
+    });
 })
 
 /**
@@ -58,30 +58,29 @@ button.addEventListener("click", () => {
  * @param {string} nasdaq 
  */
 async function fetchStock(nasdaq) {
-    let url = `http://localhost:3000/api/nasdaq/${nasdaq}`;
-    let resp = await fetch(url);
-    let content;
-    if(resp.ok) {
-        content = await resp.json();
-    }
-    else {
-        throw new Error("Status code: " + resp.status);
-    }
+  let url = `http://localhost:3000/api/nasdaq/${nasdaq}`;
+  let resp = await fetch(url);
+  let content;
+  if(resp.ok) {
+    content = await resp.json();
+  }else {
+    throw new Error("Status code: " + resp.status);
+  }
 
-    currentPriceStock(content, nasdaq);
+  currentPriceStock(content, nasdaq);
 }
 
 /**
+ 
  * function that changes the paragraph whether it shows the current price of a certain stock or it shows en error
  * @param {Object} content 
  * @param {string} nasdaq 
  */
 function currentPriceStock(content, nasdaq) {
-    let currentPrice = document.querySelector('#current_price');
-    if (content.c > 0){
-        currentPrice.textContent = `Current price of ${nasdaq} is: $${content.c}`
-    }
-    else{
-        currentPrice.textContent = "ERROR: Symbol not found, please try again"
-    }
+  let currentPrice = document.querySelector('#current_price');
+  if (content.c > 0){
+    currentPrice.textContent = `Current price of ${nasdaq} is: $${content.c}`
+  }else{
+    currentPrice.textContent = "ERROR: Symbol not found, please try again"
+  }
 }
